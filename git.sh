398849f693cfaec8581faaf47c5b18058c07d1d3
forever361 <155178977@qq.com>
2024-02-29 18:17:15 +0800

    1

---
+++ git.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+repo_path="/app/tanos/fp/Flask_tp"
+
+if [ -d "$repo_path" ]; then
+    # 仓库已存在，执行 git pull
+    echo "仓库已存在，执行 git pull..."
+    cd "$repo_path" || exit
+    git pull
+else
+    # 仓库不存在，执行 git clone
+    echo "仓库不存在，执行 git clone..."
+    git clone git@gitee.com:kundwong/Flask_tp.git "$repo_path"
+fi
